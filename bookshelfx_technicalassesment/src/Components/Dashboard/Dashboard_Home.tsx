a34b145648a3f1146067587f4068f073f0f7af6e
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-04-30 22:48:36 -0400

    Add new dependencies and update Navbar component

--- bookshelfx_technicalassesment/src/Components/Dashboard/Dashboard_Home.tsx
+++ bookshelfx_technicalassesment/src/Components/Dashboard/Dashboard_Home.tsx
@@ -12,155 +12,143 @@ import BookDetails from '../BookDetails/BookDetails';
 import { Book } from '../interfaceModels';
 import BooksData from  "../Mock-BookData.json";
 import { motion } from "framer-motion"
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
 
-export default function Dashboard_Home()
+const drawerWidth = 240;
+
+interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * Remove this when copying and pasting into your project.
+     */
+    window?: () => Window;
+  }
+
+export default function Dashboard_Home(props: Props)
 {
     const [searchLabel, setSearchLabel] = React.useState("Search for books, authors, genres...");
     const [isBookSelected, setIsBookSelected] = React.useState(false);
     const [selectedBook, setSelectedBook] = React.useState<Book | null>(null);
-
+    
     const handleLearMoreClick = (book: any) => {
         setIsBookSelected(true);
         setSelectedBook(book);    
     }
-    
+
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [isClosing, setIsClosing] = React.useState(false);
+  
+    const handleDrawerClose = () => {
+      setIsClosing(true);
+      setMobileOpen(false);
+    };
+  
+    const handleDrawerTransitionEnd = () => {
+      setIsClosing(false);
+    };
+  
+    const handleDrawerToggle = () => {
+      if (!isClosing) {
+        setMobileOpen(!mobileOpen);
+      }
+    };
+  
+    const drawer = (
+      <Box>
+        <Toolbar sx={{
+          mb: 4,
+        }}>
+          <Typography variant="h5">
+            <LocalLibraryIcon sx={{mr:1}}/>
+            BookshelfX
+          </Typography>
+        </Toolbar>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
     return(
         <ThemeProvider theme={theme}>
-            <Navbar/>
             <motion.div
                 initial={{y:20, opacity:0}}
                 animate={{y:0, opacity:1}}
                 transition={{ease: 'easeInOut', duration:0.5}}
             >
-            {isBookSelected? 
-                <BookDetails
-                    ISBN={selectedBook?.ISBN || ""}
-                    bookimage={selectedBook?.bookimage || ""}
-                    title={selectedBook?.title || ""}
-                    description={selectedBook?.description || ""}
-                    rating={selectedBook?.rating|| 0}
-                    publisher={selectedBook?.publisher|| ""}
-                    publishedDate={selectedBook?.publishedDate|| ""}
-                    category={selectedBook?.category|| ""}
-                    pageCount={selectedBook?.pageCount|| 0}
-                    customerReviews={selectedBook?.customerReviews|| ["No review available"]}
-                />:<Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: ['column', 'column'], // column layout for small screens, row layout for larger screens
-                    width: '100%',
-                    height: '100%',
-                    overflowX: 'hidden', // disable horizontal scrolling
-                    overflowY: 'hidden', // disable vertical scrolling
-                    margin: 0, // remove margin
-                    padding: 0, // remove padding
-                }}
-            >
-                <Box sx={{
-                    position: 'relative', // make this box positioned
-                    width: ['100%', '100%'], // full width on small screens, half width on larger screens
-                    display: 'flex',
-                    minHeight: ['80vh','80vh'],
-                    backgroundImage: `url(${DashBoardImage.src})`, 
-                    backgroundSize: 'cover',
-                    backgroundImagePosition: 'center',
-                    backgroundRepeat: 'no-repeat',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    alignContent: 'center',
-                    zIndex: -1,
-                    '&::before': {
-                        content: '""',
-                        position: 'absolute',
-                        top: 0,
-                        right: 0,
-                        bottom: 0,
-                        left: 0,
-                        background: 'rgba(255, 255, 240, 0.3)',  
-                        backdropFilter: 'blur(5px)',
-                      },
-                }}>
-                </Box>
-                <Box sx={{ 
-                    width: ['60%','75%'],
-                    zIndex: 1, 
-                    position: 'absolute',
-                    top: '50%', 
-                    left: '50%', 
-                    transform: 'translate(-50%, -50%)', 
-                    textAlign: 'center' }}
+                <CssBaseline />
+                <Navbar/>
+                <Box
+                    component="nav"
+                    sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                    aria-label="mailbox folders"
                 >
-                    <Typography variant="h3" letterSpacing={4} sx={{fontWeight:"bold"}}> 
-                         &quot;Discover, Explore, Read: Your Personal Gateway to Infinite Worlds&quot;
-                    </Typography>
-                    <Typography variant="body1" letterSpacing={1} sx={{fontWeight:"bold"}}> 
-                        Welcome to BookshelfX: your gateway to infinite worlds. Explore captivating stories and discover new adventures with ease.
-                    </Typography>
-                     
-                    <TextField 
-                        id="input-with-icon-textfield" 
-                        fullWidth
-                        label={searchLabel}
-                        variant="outlined"
-                        onFocus={() => setSearchLabel("")} // Change the label text when the TextField is focused
-                        onBlur={() => setSearchLabel("Search for books, authors, genres...")} // Change the label text based on the screen size when the TextField loses focus
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <SavedSearchIcon sx={{
-                                        fontSize:"3rem",
-                                        color: theme.palette.text.secondary,
-                                        zIndex: 1
-                                    }}/>
-                                </InputAdornment>
-                            ),
-                        }}
-                        sx={{
-                            width:"100%",
-                            mt:5,
-                            mb:5,
-                            color: 'black', // Change the color of the text to black
-                            '& .MuiOutlinedInput-root': { // Change the border radius of the TextField
-                                borderRadius: '50px',
-                                '&.Mui-focused': {
-                                    color: 'black', // Change the color of the text to black when the TextField is focused
-                                },
-                            },
-                            '& .MuiOutlinedInput-notchedOutline': { // Change the border color of the TextField
-                                borderColor: 'white',
-                                backgroundColor: 'rgba(255,255,255,0.7)', // Make the background less opaque
-                            },
-                            '& .MuiOutlinedInput-input': { // Change the color of the input text when the TextField is focused
-                                '&.Mui-focused': {
-                                    color: 'black',
-                                },
-                            },
-                        }}
-                    />
-                </Box>
-                <Box sx={{
-                    display: 'flex',
-                    flexDirection: ['column', 'row'], // column layout for small screens, row layout for larger screens
-                    flexWrap: 'wrap', // allow cards to wrap to the next line\
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    alignContent: 'center',
-                    width: '100%',
-                    height: '100%',
-                    mt: 3,
-                }}>
-                    {BooksData.map((book, index) => (
-                        <BookCard
-                            key={index}
-                            image={book.bookimage}
-                            title={book.title}
-                            description={book.description}
-                            rating={book.rating}
-                            onLearnMore={() => handleLearMoreClick(book)}
-                        />
-                    ))}
-                </Box>
-            </Box>}
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                container={container}
+                variant="temporary"
+                open={mobileOpen}
+                onTransitionEnd={handleDrawerTransitionEnd}
+                onClose={handleDrawerClose}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+                sx={{
+                    display: { xs: 'block', sm: 'none' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+                >
+                {drawer}
+                </Drawer>
+                <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+                open
+                >
+                {drawer}
+                </Drawer>
+            </Box>
             </motion.div>
         </ThemeProvider>
     )
--- bookshelfx_technicalassesment/src/Components/Navbar/Navbar.tsx
+++ bookshelfx_technicalassesment/src/Components/Navbar/Navbar.tsx
@@ -8,21 +8,171 @@ import IconButton from '@mui/material/IconButton';
 import theme from '../Themes';
 import {ThemeProvider} from '@mui/material/styles';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { Button, CssBaseline } from '@mui/material';
+import { alpha, styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import {User} from "../interfaceModels"
+import Cookies from 'js-cookie';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import '@fontsource/caveat-brush';
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
+const drawerWidth = 240;
 
 export default function Navbar()
 {
+    const user: User | null = Cookies.get('user') ? JSON.parse(Cookies.get('user')!) : null;
+    
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const options = {
+      "Profile": 0,
+      "Logout": 1,
+    }
+    
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      }));
+      
+      const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }));
+      
+      const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        width: '100%',
+        '& .MuiInputBase-input': {
+          padding: theme.spacing(1, 1, 1, 0),
+          // vertical padding + font size from searchIcon
+          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+          transition: theme.transitions.create('width'),
+          [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+              width: '20ch',
+            },
+          },
+        },
+      }));
+
+
+
+    console.log(user ? user.name.charAt(0).toUpperCase() + user.name.slice(1).toLowerCase() : "Guest");
     return(
         <ThemeProvider theme={theme}>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position='relative'>
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            BookshelfX
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-        </ThemeProvider>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{
+                width: { sm: `calc(100% - ${drawerWidth}px)` },
+                ml: { sm: `calc(${drawerWidth}px)` },
+                }}
+            >
+            <Toolbar>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
+              
+              <Box sx={{ flexGrow: 1 }} />
+              
+              <IconButton color="inherit">
+                <Badge badgeContent={0} color="error">
+                  <NotificationsIcon 
+                    sx={{ 
+                      borderRadius: '50%', // Make it circular
+                      transition: 'background-color 0.3s', // Smooth transition
+                      '&:hover': {
+                        backgroundColor: 'rgba(0, 0, 0, 0.08)', // Change background color on hover
+                      },
+                    }}
+                  />
+                </Badge>
+              </IconButton>
+
+              <Button 
+                variant="text"
+                sx={{ 
+                  ml: 'auto', 
+                  color:'white',
+                  fontFamily: "'Caveat Brush', cursive",
+                  '&:hover': {
+                    backgroundColor: 'rgba(0, 0, 0, 0.08)',
+                  },
+                  m:1
+                }}
+                onClick={handleClick}
+              >
+                <Typography 
+                  variant="h6" 
+                  noWrap 
+                  component="div" 
+                  letterSpacing={3} 
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center', // Center items horizontally
+                    alignItems: 'center', // Center items vertically
+                    fontFamily: "'Caveat Brush', cursive",
+                    fontSize: '1.5rem',
+                    fontWeight: '400'
+                  }}
+                >
+                  {user ? user.name.charAt(0).toUpperCase() + user.name.slice(1).toLowerCase() : "Guest"}
+                  <ArrowDropDownIcon sx={{
+                    fontSize: '1.5rem',
+                    ml: 0.5
+                  }}/>
+                </Typography>
+              </Button>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+
+            </Toolbar>
+            </AppBar>
+      </ThemeProvider>
     );
 }
 
