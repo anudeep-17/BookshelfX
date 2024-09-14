f1ce2f1e3cdf966f44c58e60638110e5aad8aa41
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-09-13 22:45:19 -0400

    Update Prisma schema and migrations

--- bookshelfx_technicalassesment/prisma/migrations/20240603080137_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "BookRentalDetails" ADD COLUMN     "librarianId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "BookRentalDetails" ADD CONSTRAINT "BookRentalDetails_librarianId_fkey" FOREIGN KEY ("librarianId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
