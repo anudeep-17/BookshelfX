d2b5c40f03f9c3c5bec5b0f61c15ed93644b7359
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-09-14 02:46:53 -0400

    Refactor API route functions to use generic Request type

--- bookshelfx_technicalassesment/src/app/api/books/librarian/GoogleAPISupport/getBooksbyAuthorFromGoogleApi/route.ts
+++ bookshelfx_technicalassesment/src/app/api/books/librarian/GoogleAPISupport/getBooksbyAuthorFromGoogleApi/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import { NextApiRequest } from "next";
-
-export async function GET(req: NextApiRequest)
+ 
+export async function GET(req: Request)
 {   
     const url = new URL(req.url || '');
     const author = url.searchParams.get('author');
