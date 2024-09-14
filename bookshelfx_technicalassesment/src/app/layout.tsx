4612b93c7f187ae699484d544ecfd07a97aacbdc
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-09-14 03:54:31 -0400

    Add @vercel/speed-insights package

--- bookshelfx_technicalassesment/src/app/layout.tsx
+++ bookshelfx_technicalassesment/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
           <Analytics/>
+          <SpeedInsights/>
           {children}
       </body>
     </html>
